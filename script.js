ff9b784218d1a36307f6f9ed510532a638a981e7
JokubasKitm <vladislovas.jokubas.razas@stud.kitm.lt>
2024-11-21 15:18:26 +0200

    comit

---
+++ script.js
@@ -0,0 +1,43 @@
+"use strict"
+
+// 1 uzd
+
+// let number = 4
+// let numberOfNumbers = 0
+
+// while (number < 50) {
+//     console.log(number)
+//     number += 4
+//     numberOfNumbers += 1
+// }
+
+// console.log(`number of numbers is: ${numberOfNumbers}`)
+
+/////////////////////////////////////////////////////////////
+
+// 2 uzd
+
+// const words = ['apples', 'pear', 'dragonfruit', 'banana', 'lemons']
+// let wordLength = []
+
+// words.forEach((element) => wordLength.push(element.length))
+
+// console.log(words)
+// console.log(wordLength)
+
+/////////////////////////////////////////////////////////////
+
+// 3 uzd
+
+const musician = {
+    firstName: 'joe',
+    lastName: 'jimmy',
+    instrument: 'drums',
+    albums: {
+        first: 'first album',
+        second: 'second album',
+        third: 'third album'
+    }
+}
+
+console.log(musician.albums)
